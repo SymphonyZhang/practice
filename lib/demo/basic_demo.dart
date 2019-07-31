ef905a93b3c562e690d925356a89988e78dd7dfe
Symphony <95200261@qq.com>
2019-07-31 16:36:58 +0800

    装饰盒子Container

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -9,14 +9,18 @@ class BasicDemo extends StatelessWidget {
     return Container(
       color: Colors.grey[100],
       child: Row(
+        //主轴居中对齐
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Container(
             child: Icon(Icons.pool,color: Colors.white,),
-            color: Color.fromRGBO(3, 54, 255, 1.0),
             padding: EdgeInsets.all(16.0),
             margin: EdgeInsets.all(8.0),
             width: 90.0,
             height: 90.0,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+            ),
           ),
         ],
       ),

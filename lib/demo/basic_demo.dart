84ddd6281914c9f0989561163bf963b92ca703b8
Symphony <95200261@qq.com>
2019-07-31 17:10:34 +0800

    形状 shape

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -21,20 +21,20 @@ class BasicDemo extends StatelessWidget {
             height: 90.0,
             //详细设置容器样式，如果有decoration则，在decoration上一层不能出现color，会冲突
             decoration: BoxDecoration(
-              color: Color.fromRGBO(3, 54, 255, 1.0),
-              //添加统一描边
-              border: Border.all(
-                //设置全部描边
-                //描边的颜色
-                color: Colors.indigoAccent[100],
-                //描边的粗细
-                width: 3.0,
-                //描边的样式，实心的
-                style: BorderStyle.solid,
-              ),
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                //添加统一描边
+                border: Border.all(
+                  //设置全部描边
+                  //描边的颜色
+                  color: Colors.indigoAccent[100],
+                  //描边的粗细
+                  width: 3.0,
+                  //描边的样式，实心的
+                  style: BorderStyle.solid,
+                ),
 
-              //可以分开设置描边
-              /*border: Border(
+                //可以分开设置描边
+                /*border: Border(
                 top: BorderSide(
                   //描边的颜色
                   color: Colors.indigoAccent[100],
@@ -53,27 +53,29 @@ class BasicDemo extends StatelessWidget {
                 ),
               ),*/
 
-              //设置圆角全局
-              borderRadius: BorderRadius.circular(16.0),
-              //分别设置圆角
-              /*borderRadius: BorderRadius.only(
+                //设置圆角全局
+                //borderRadius: BorderRadius.circular(16.0),
+                //分别设置圆角
+                /*borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(64.0),
                 bottomLeft: Radius.circular(64.0),
               ),*/
-              //阴影
-              boxShadow: [
-                BoxShadow(
-                  //偏移
-                  offset: Offset(0.0, 16.0),
-                  //设置阴影颜色
-                  color: Color.fromRGBO(16, 20, 188, 1.0),
-                  //设置阴影模糊程度，值越大，越模糊
-                  blurRadius: 25.0,
-                  //设置阴影扩散程度  正数：扩大阴影面积 负数：缩小阴影面积
-                  spreadRadius: -9.0,
-                ),
-              ],
-            ),
+                //阴影
+                boxShadow: [
+                  BoxShadow(
+                    //偏移
+                    offset: Offset(0.0, 16.0),
+                    //设置阴影颜色
+                    color: Color.fromRGBO(16, 20, 188, 1.0),
+                    //设置阴影模糊程度，值越大，越模糊
+                    blurRadius: 25.0,
+                    //设置阴影扩散程度  正数：扩大阴影面积 负数：缩小阴影面积
+                    spreadRadius: -9.0,
+                  ),
+                ],
+                //形状   ps:在圆形的盒子外面不能使用圆角效果 BoxShape.circle 跟BorderRadius冲突
+                //rectangle矩形  circle圆形
+                shape: BoxShape.circle),
           ),
         ],
       ),

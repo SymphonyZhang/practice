493ee48f4844870b035f12465bc7111925493ad7
Symphony <95200261@qq.com>
2019-07-31 14:02:55 +0800

    添加标题栏下方显示的tab

--- lib/main.dart
+++ lib/main.dart
@@ -26,37 +26,47 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      //app标题栏
-      appBar: AppBar(
-        //添加标题栏左边的menu button
-        leading: IconButton(
-          //图标
-          icon: Icon(Icons.menu),
-          //描述
-          tooltip: 'Navigation',
-          //点击事件
-          onPressed: () => debugPrint('This is Navigation bar menu'),
-        ),
-        title: Text('Practice'),
-        //添加标题栏右边的actions button
-        actions: <Widget>[
-          //在actions中可以添加多个IconButton
-          //图标按钮部件
-          IconButton(
+    return DefaultTabController(
+      //设置tab的个数
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        //app标题栏
+        appBar: AppBar(
+          //添加标题栏左边的menu button
+          leading: IconButton(
             //图标
-            icon: Icon(Icons.search),
+            icon: Icon(Icons.menu),
             //描述
-            tooltip: 'Search',
+            tooltip: 'Navigation',
             //点击事件
-            onPressed: () => debugPrint('Search button had pressed'),
+            onPressed: () => debugPrint('This is Navigation bar menu'),
           ),
-        ],
-        //设置阴影大小，默认4.0
-        elevation: 0.0,
+          title: Text('Practice'),
+          //添加标题栏右边的actions button
+          actions: <Widget>[
+            //在actions中可以添加多个IconButton
+            //图标按钮部件
+            IconButton(
+              //图标
+              icon: Icon(Icons.search),
+              //描述
+              tooltip: 'Search',
+              //点击事件
+              onPressed: () => debugPrint('Search button had pressed'),
+            ),
+          ],
+          //设置阴影大小，默认4.0
+          elevation: 0.0,
+          //添加Tab
+          bottom: TabBar(tabs: <Widget>[
+            Tab(icon: Icon(Icons.local_florist),),
+            Tab(icon: Icon(Icons.change_history),),
+            Tab(icon: Icon(Icons.directions_bike),),
+          ]),
+        ),
+        body: null,
       ),
-      body: null,
     );
   }
 }

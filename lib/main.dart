ce53708f3f78bc8fdc12328d00dc541ed492174a
Symphony <95200261@qq.com>
2019-07-31 14:09:52 +0800

    用TabBarView部件添加tab 对应的视图view显示

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,14 @@ class Home extends StatelessWidget {
             Tab(icon: Icon(Icons.directions_bike),),
           ]),
         ),
-        body: null,
+        //用TabBarView部件添加tab 对应的视图view显示
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist,size: 128.0,color: Colors.black12,),
+            Icon(Icons.change_history,size: 128.0,color: Colors.black12,),
+            Icon(Icons.directions_bike,size: 128.0,color: Colors.black12,),
+          ],
+        ),
       ),
     );
   }

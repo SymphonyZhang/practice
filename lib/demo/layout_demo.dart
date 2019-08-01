f7fda70d2cd18f7cba2b4aa4b8379841dfb6d84b
Symphony <95200261@qq.com>
2019-08-01 11:11:40 +0800

    SizedBox 固定尺寸的盒子

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -27,18 +27,43 @@ class LayoutDemo extends StatelessWidget {
       //主轴： 在Column 中，主轴是纵向的
       //交叉轴：crossAxis 在Column中 主轴是横向的
       child: Column(
-        //设置部件在主轴上的对齐方式
-        //MainAxisAlignment.spaceAround:剩余空间分布在小部件周围
-        //MainAxisAlignment.spaceBetween:剩余空间平均分布在小部件之间
-        //MainAxisAlignment.spaceEvenly:剩余空间平均分布在小部件之间包括前后两个小部件之外也有空间
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //设置部件在交叉轴上的对齐方式
-        //CrossAxisAlignment.stretch:拉伸小部件
-        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          IconBadge(Icons.pool),
-          IconBadge(Icons.beach_access,size: 64.0,),
-          IconBadge(Icons.airplanemode_active),
+          //可以强制它的子部件有一个固定的尺寸，如果不设置尺寸则它的大小就是他的子部件的大小
+          SizedBox(
+            width: 200.0,
+            height: 300.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Icon(
+                Icons.ac_unit,
+                color: Colors.white,
+                size: 32.0,
+              ),
+            ),
+          ),
+          //不设置他的child小部件，可以当作控件间的间距
+          SizedBox(
+            height: 32.0,
+          ),
+          SizedBox(
+            width: 100.0,
+            height: 100.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Icon(
+                Icons.brightness_2,
+                color: Colors.white,
+                size: 32.0,
+              ),
+            ),
+          ),
         ],
       ),
     );

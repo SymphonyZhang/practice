4a7cd25f5f636bc4aeaae15173f15bb1a6347369
Symphony <95200261@qq.com>
2019-08-01 15:28:12 +0800

    实现基础简单的PageView

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -4,6 +4,32 @@ class ViewDemo extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container();
+    return PageViewDemo();
+  }
+}
+
+//PageView 页面视图
+class PageViewDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          color: Colors.brown[900],
+          child: Text('One',style: TextStyle(fontSize: 32.0,color: Colors.white),),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.grey[900],
+          child: Text('Two',style: TextStyle(fontSize: 32.0,color: Colors.white),),
+        ),
+        Container(
+          alignment: Alignment.center,
+          color: Colors.blueGrey[900],
+          child: Text('Three',style: TextStyle(fontSize: 32.0,color: Colors.white),),
+        ),
+      ],
+    );
   }
 }

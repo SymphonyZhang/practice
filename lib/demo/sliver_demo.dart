0abca99ffc86f41e44579b9cd7259f4aed4d35b4
Symphony <95200261@qq.com>
2019-08-01 17:50:11 +0800

    Sliver

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
+//sliver 指一小块的可视口
 class SliverDemo extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+
+        ],
+      ),
+    );
   }
 }

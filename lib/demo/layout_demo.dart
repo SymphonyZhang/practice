cac77421ce55e34d8a9159682bd9c8aee79f6bdb
Symphony <95200261@qq.com>
2019-08-01 11:01:41 +0800

    crossAxis 交叉轴

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,29 +7,37 @@ class LayoutDemo extends StatelessWidget {
     return Container(
       //横排
       //主轴：在Row中主轴是横向的，
+      //交叉轴：crossAxis 在Row中 主轴是纵向的
       child: Row(
         //设置部件在主轴上的对齐方式
         //MainAxisAlignment.spaceAround:剩余空间分布在小部件周围
         //MainAxisAlignment.spaceBetween:剩余空间平均分布在小部件之间
         //MainAxisAlignment.spaceEvenly:剩余空间平均分布在小部件之间包括前后两个小部件之外也有空间
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //设置部件在交叉轴上的对齐方式
+        //CrossAxisAlignment.stretch:拉伸小部件
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           IconBadge(Icons.pool),
-          IconBadge(Icons.beach_access),
+          IconBadge(Icons.beach_access,size: 64.0,),
           IconBadge(Icons.airplanemode_active),
         ],
       ),
       //竖排
       //主轴： 在Column 中，主轴是纵向的
+      //交叉轴：crossAxis 在Column中 主轴是横向的
       /*child: Column(
         //设置部件在主轴上的对齐方式
         //MainAxisAlignment.spaceAround:剩余空间分布在小部件周围
         //MainAxisAlignment.spaceBetween:剩余空间平均分布在小部件之间
         //MainAxisAlignment.spaceEvenly:剩余空间平均分布在小部件之间包括前后两个小部件之外也有空间
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //设置部件在交叉轴上的对齐方式
+        //CrossAxisAlignment.stretch:拉伸小部件
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           IconBadge(Icons.pool),
-          IconBadge(Icons.beach_access),
+          IconBadge(Icons.beach_access,size: 64.0,),
           IconBadge(Icons.airplanemode_active),
         ],
       ),*/

436a41077ec44b59c909b93fce60a135f08fe2cd
Symphony <95200261@qq.com>
2019-08-01 18:05:49 +0800

    SliverPadding 内边距 SliverSafeArea 安全区域

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -9,31 +9,45 @@ class SliverDemo extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          //Sliver 构建GridView
-          SliverGrid(
-            //实现GridView里的item
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  child: Image.network(
-                    posts[index].imageUrl,
-                    fit: BoxFit.cover,
-                  ),
-                );
-              },
-              //设置items的个数
-              childCount: posts.length,
-            ),
-            //设置网格属性
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.0,
-              mainAxisSpacing: 8.0,
-              //控制item显示比例
-              childAspectRatio: 1.0,
+          //内容显示在安全区域
+          SliverSafeArea(
+            //添加内边距
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(8.0),
+              sliver: SliverGridDemo(),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    //Sliver 构建GridView
+    return SliverGrid(
+      //实现GridView里的item
+      delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+          return Container(
+            child: Image.network(
+              posts[index].imageUrl,
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        //设置items的个数
+        childCount: posts.length,
+      ),
+      //设置网格属性
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        //控制item显示比例
+        childAspectRatio: 1.0,
       ),
     );
   }

78cba598165a8c04fdc06924fe6756d89be5cf10
Symphony <95200261@qq.com>
2019-08-01 18:01:04 +0800

    SliverGrid 网格视图

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 //sliver 指一小块的可视口
-class SliverDemo extends StatelessWidget{
+class SliverDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-
+          //Sliver 构建GridView
+          SliverGrid(
+            //实现GridView里的item
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  child: Image.network(
+                    posts[index].imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+              //设置items的个数
+              childCount: posts.length,
+            ),
+            //设置网格属性
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+              //控制item显示比例
+              childAspectRatio: 1.0,
+            ),
+          ),
         ],
       ),
     );

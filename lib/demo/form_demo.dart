0e9edee3129d0c77364b3cc2161d9c6f9c63b6b1
Symphony <95200261@qq.com>
2019-08-05 17:22:44 +0800

    Form 添加一个简单表单

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -7,14 +7,14 @@ class FormDemo extends StatelessWidget {
       body: Theme(
         //使用ThemeData则覆盖之前所有主题设置，使用Theme.of(context).copyWith则只覆盖修改的这部分设置
         data: Theme.of(context).copyWith(
-          primaryColor:Colors.black,
+          primaryColor: Colors.black,
         ),
         child: Container(
           padding: EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextFieldDemo(),
+              RegisterForm(),
             ],
           ),
         ),
@@ -23,6 +23,51 @@ class FormDemo extends StatelessWidget {
   }
 }
 
+class RegisterForm extends StatefulWidget {
+  @override
+  RegisterFormState createState() => RegisterFormState();
+}
+
+class RegisterFormState extends State<RegisterForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Username',
+            ),
+          ),
+          TextFormField(
+            //密文显示小圆点
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+            ),
+          ),
+          SizedBox(
+            height: 32.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text(
+                'Register',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              elevation: 0.0,
+              onPressed: (){},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 //输入文本框
 class TextFieldDemo extends StatefulWidget {
@@ -31,7 +76,6 @@ class TextFieldDemo extends StatefulWidget {
 }
 
 class TextFieldDemoState extends State<TextFieldDemo> {
-
   //文本编辑控制器
   final textEditingController = TextEditingController();
 
@@ -46,11 +90,9 @@ class TextFieldDemoState extends State<TextFieldDemo> {
     //设置文本框默认值
     //textEditingController.text="hi";
     //设置文本框监听器
-    textEditingController.addListener(
-        (){
-          debugPrint('input： ${textEditingController.text}');
-        }
-    );
+    textEditingController.addListener(() {
+      debugPrint('input： ${textEditingController.text}');
+    });
     super.initState();
   }
 
@@ -63,13 +105,13 @@ class TextFieldDemoState extends State<TextFieldDemo> {
       },*/
       controller: textEditingController,
       //监视键盘上submit按钮
-      onSubmitted: (value){
+      onSubmitted: (value) {
         debugPrint('submit: $value');
       },
       decoration: InputDecoration(
-        icon:Icon(Icons.subject),
+        icon: Icon(Icons.subject),
         labelText: 'Title',
-        hintText:'Enter the post title.',
+        hintText: 'Enter the post title.',
         //默认状态下，去除输入框下边
         //border: InputBorder.none,
         //在四周添加统一的边框
@@ -81,8 +123,6 @@ class TextFieldDemoState extends State<TextFieldDemo> {
     );
   }
 }
-
-
 
 class ThemeDemo extends StatelessWidget {
   @override

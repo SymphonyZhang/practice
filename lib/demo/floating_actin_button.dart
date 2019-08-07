322ae832a771d2605aecd8f5d0f5407b70492953
Symphony <95200261@qq.com>
2019-08-07 15:53:51 +0800

    整理demo代码

---
+++ lib/demo/floating_actin_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonDemo extends StatelessWidget {
+  final Widget _floatingActionButton = FloatingActionButton(
+    onPressed: () {},
+    child: Icon(Icons.add),
+    elevation: 0.0,
+    backgroundColor: Colors.black87,
+    //方形
+    /*shape: BeveledRectangleBorder(
+      //30.0会变成菱形
+      borderRadius: BorderRadius.circular(30.0),
+    ),*/
+  );
+
+  final Widget _floatingActionButtonExtended = FloatingActionButton.extended(
+    onPressed: () {},
+    icon: Icon(Icons.add),
+    label: Text('Add'),
+    elevation: 0.0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FloatingActionButtonDemo'),
+        elevation: 0.0,
+      ),
+      floatingActionButton: _floatingActionButton,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 80.0,
+        ),
+        shape: CircularNotchedRectangle(),
+      ),
+    );
+  }
+}

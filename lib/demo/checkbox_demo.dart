5a66bbbe1b8a145d7b35dee35a2cdad9916475d7
Symphony <95200261@qq.com>
2019-08-07 17:54:48 +0800

    Checkbox 复选框

--- lib/demo/checkbox_demo.dart
+++ lib/demo/checkbox_demo.dart
@@ -6,6 +6,8 @@ class CheckboxDemo extends StatefulWidget {
 }
 
 class CheckboxDemoState extends State<CheckboxDemo> {
+  bool _checkboxItemA = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +22,17 @@ class CheckboxDemoState extends State<CheckboxDemo> {
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[],
+              children: <Widget>[
+                Checkbox(
+                  value: _checkboxItemA,
+                  onChanged: (value){
+                    setState(() {
+                      _checkboxItemA = value;
+                    });
+                  },
+                  activeColor: Colors.black,
+                ),
+              ],
             ),
           ],
         ),

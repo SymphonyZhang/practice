305ee3ff61aedb417b20b87818bf5508d5235b27
Symphony <95200261@qq.com>
2019-08-30 16:10:53 +0800

    SwitchListTile 带标签与图标的开关

--- lib/demo/switch_demo.dart
+++ lib/demo/switch_demo.dart
@@ -20,10 +20,23 @@ class SwitchDemoState extends State<SwitchDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SwitchListTile(
+              value: _switchItemA,
+              onChanged: (value) {
+                setState(() {
+                  _switchItemA = value;
+                });
+              },
+              title: Text('Switch Item A'),
+              subtitle: Text('Description'),
+              secondary:
+                  Icon(_switchItemA ? Icons.visibility : Icons.visibility_off),
+              selected: _switchItemA,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(_switchItemA ? '😁' : '😐',style: TextStyle(fontSize: 32.0),),
+                /*Text(_switchItemA ? '😁' : '😐',style: TextStyle(fontSize: 32.0),),
                 Switch(
                   value: _switchItemA,
                   onChanged: (value) {
@@ -31,7 +44,7 @@ class SwitchDemoState extends State<SwitchDemo> {
                       _switchItemA = value;
                     });
                   },
-                ),
+                ),*/
               ],
             ),
           ],

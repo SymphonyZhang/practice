3b1bf084e85c3cef7ed585ee35f135b0eb456c90
Symphony <95200261@qq.com>
2019-08-30 16:33:18 +0800

    Slider 滑动选择器

--- lib/demo/slider_demo.dart
+++ lib/demo/slider_demo.dart
@@ -6,6 +6,7 @@ class SliderDemo extends StatefulWidget {
 }
 
 class SliderDemoState extends State<SliderDemo> {
+  double _sliderItemA = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +22,26 @@ class SliderDemoState extends State<SliderDemo> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-
+                Slider(
+                  value: _sliderItemA,
+                  onChanged: (value){
+                    setState(() {
+                      _sliderItemA = value;
+                    });
+                  },
+                  activeColor: Theme.of(context).accentColor,//滑动后和滑块的颜色
+                  inactiveColor: Theme.of(context).accentColor.withOpacity(0.3),//未滑动部分的颜色
+                  min: 0.0,//最小值
+                  max: 10.0,// 最大值
+                  divisions: 10,//分割份数
+                  label: '${_sliderItemA.toInt()}',// 标签
+                ),
               ],
-            )
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text('SliderValue: $_sliderItemA'),
           ],
         ),
       ),

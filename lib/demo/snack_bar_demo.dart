5af32d06349cc7888150154770d9a15bf6037403
Symphony <95200261@qq.com>
2019-09-16 16:27:15 +0800

    Snack Bar 操作提示栏

--- lib/demo/snack_bar_demo.dart
+++ lib/demo/snack_bar_demo.dart
@@ -21,12 +21,34 @@ class SnackBarDemoState extends State<SnackBarDemo> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-
+                SnackBarButton(),
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class SnackBarButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text('Open Snack Bar'),
+      onPressed: (){
+        Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Processing...'),
+              action: SnackBarAction(
+                label: 'OK',
+                onPressed: (){
+
+                },
+              ),
+            )
+        );
+      },
     );
   }
 }

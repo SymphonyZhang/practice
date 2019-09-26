9324513950255ef9c100d175464bfc892c09184b
Symphony <95200261@qq.com>
2019-09-26 17:36:39 +0800

    DataTable 数据表格

--- lib/demo/data_table_demo.dart
+++ lib/demo/data_table_demo.dart
@@ -15,13 +15,36 @@ class DataTableDemoState extends State<DataTableDemo> {
       ),
       body: Container(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-
+            DataTable(
+              columns: [
+                DataColumn(
+                  label: Text('Title'),
+                ),
+                DataColumn(
+                  label: Text('Author'),
+                ),
+              ],
+              rows: [
+                DataRow(
+                  cells: [
+                    DataCell(Text('hello~')),
+                    DataCell(Text('Symphony')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('hola~')),
+                    DataCell(Text('Symphony')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('你好~')),
+                    DataCell(Text('Symphony')),
+                  ],
+                ),
               ],
             ),
           ],

de7baae81ab9de43fe2d8954734df83c489e0ac5
Symphony <95200261@qq.com>
2019-10-11 14:09:23 +0800

    使用Subjects控制Observables

--- lib/demo/rxdart/rxdart_demo.dart
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -30,9 +30,18 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
     //Observable<String> _observable = Observable.fromFuture(Future.value('Hello ~'));
     //Observable<String> _observable = Observable.fromIterable(['hello','你好']);
     //Observable<String> _observable = Observable.just('Hello ~');
-    Observable<String> _observable = Observable.periodic(Duration(seconds: 3),(x) => x.toString());
+    //Observable<String> _observable = Observable.periodic(Duration(seconds: 3),(x) => x.toString());
 
-    _observable.listen(print);
+    //_observable.listen(print);
+
+    PublishSubject<String> _subject = PublishSubject<String>();
+
+    _subject.listen((data) => print('Listen 1 : $data'));
+    _subject.add('hello');
+    _subject.listen((data) => print('Listen 2 : ${data.toUpperCase()}'));
+    _subject.add('hola');
+
+    _subject.close();
   }
 
   @override

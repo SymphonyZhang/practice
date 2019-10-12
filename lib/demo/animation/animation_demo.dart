c486e88bd9f9a028989ab1ced4ce2e52ca5bcb80
Symphony <95200261@qq.com>
2019-10-12 11:52:54 +0800

    AnimationController: 动画控制器

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -18,7 +18,28 @@ class AnimationDemoHome extends StatefulWidget {
   _AnimationDemoHomeState createState() => _AnimationDemoHomeState();
 }
 
-class _AnimationDemoHomeState extends State<AnimationDemoHome> {
+class _AnimationDemoHomeState extends State<AnimationDemoHome> with TickerProviderStateMixin{
+  AnimationController animationDemoController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationDemoController = AnimationController(
+        duration: Duration(milliseconds: 1000),
+        vsync: this);
+    animationDemoController.addListener((){
+      print('${animationDemoController.value}');
+    });
+
+    animationDemoController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationDemoController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();

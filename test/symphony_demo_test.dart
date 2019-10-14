b119288e8aa44956260d9e7c8a18aebc953090d6
Symphony <95200261@qq.com>
2019-10-14 17:53:48 +0800

    测试小部件的tap行为

--- test/symphony_demo_test.dart
+++ test/symphony_demo_test.dart
@@ -5,7 +5,7 @@ import 'package:practice/demo/test/test_demo.dart';
 void main() {
   test('should return hello + something.',(){
     var string = SymphonyTestDemo.greet('Symphony');
-    expect(string, 'hello Symphony ~~');
+    expect(string, 'hello Symphony ~~');//断言 判断期望值与运行值是否一致
   });
 
   testWidgets('widget testing demo.', (WidgetTester tester) async{
@@ -20,5 +20,16 @@ void main() {
     //expect(labelText, findsNothing);
     //expect(labelText, findsOneWidget);
     expect(labelText, findsNWidgets(1));
+
+    final actionChipLabelText = find.text('0');
+    expect(actionChipLabelText, findsOneWidget);
+
+    final actionChip = find.byType(ActionChip);
+    await tester.tap(actionChip);
+    await tester.pump();
+
+    final actionChipLabelTextAfterTap = find.text('1');
+    expect(actionChipLabelTextAfterTap, findsOneWidget);
+    expect(actionChipLabelText, findsNothing);
   });
 }

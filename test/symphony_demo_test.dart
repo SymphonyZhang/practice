8a1a4c5db2548933d24c94e04072ea980df5ed48
Symphony <95200261@qq.com>
2019-10-14 17:38:24 +0800

    Widget Testing:测试小部件

--- test/symphony_demo_test.dart
+++ test/symphony_demo_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:practice/demo/test/test_demo.dart';
 
@@ -5,5 +6,13 @@ void main() {
   test('should return hello + something.',(){
     var string = SymphonyTestDemo.greet('Symphony');
     expect(string, 'hello Symphony ~~');
+  });
+
+  testWidgets('widget testing demo.', (WidgetTester tester) async{
+    await tester.pumpWidget(
+      MaterialApp(
+        home: TestDemo(),
+      )
+    );
   });
 }

3e1509cf5c578f12bba1557330e53ca77fd89c0e
Symphony <95200261@qq.com>
2019-10-14 17:31:19 +0800

    运行测试

--- lib/demo/test/test_demo.dart
+++ lib/demo/test/test_demo.dart
@@ -14,6 +14,6 @@ class TestDemo extends StatelessWidget {
 
 class SymphonyTestDemo{
   static greet(String name){
-    return 'hello $name';
+    return 'hello $name ~~';
   }
 }
--- test/symphony_demo_test.dart
+++ test/symphony_demo_test.dart
@@ -4,6 +4,6 @@ import 'package:practice/demo/test/test_demo.dart';
 void main() {
   test('should return hello + something.',(){
     var string = SymphonyTestDemo.greet('Symphony');
-    expect(string, 'hello Symphony');
+    expect(string, 'hello Symphony ~~');
   });
 }

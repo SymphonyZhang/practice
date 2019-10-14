c3a69d1279f013241f583cda11bc565bbacb17db
Symphony <95200261@qq.com>
2019-10-14 09:21:38 +0800

    Intl:定义需要翻译的信息

---
+++ lib/demo/i18n/intl/symphony_demo_localizations.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class SymphonyDemoLocalizations {
+  String get title =>
+      Intl.message(
+        'hello',
+        name: 'title',
+        desc: 'demo localizations.',
+      );
+
+  String greet(String name) =>
+      Intl.message(
+        'hello $name',
+        name: 'greet',
+        desc: 'greet someone.',
+        args: [name],
+      );
+}
